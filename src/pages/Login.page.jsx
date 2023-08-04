bf97a582afa46184d509379ce3d7b26593f8acb0
WahyuIndG <wahyuig03@gmail.com>
2023-08-04 11:22:43 +0700

    update style css, for the height of page wrapper element !

--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function LoginPage() {
   return (
     <>
-    <div className='w-2/3 m-auto h-[calc(100vh-96px)] flex  '> 
-      <div className='flex flex-col m-auto'>
-        <h1 className=' text-center'>✏️ LOGIN</h1>
-        <form className='flex  flex-col  items-center h-52 justify-evenly '>
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" required />
-          <input type="password" placeholder="Type here" className="input input-bordered w-full max-w-xs" required/>
-          <button className="w-80 h-9 bg-black rounded-lg ">Wide</button>
-        </form>
+      <div className="w-2/3 m-auto h-[calc(100vh-96px)] flex  ">
+        <div className="flex flex-col m-auto">
+          <h1 className=" text-center">✏️ LOGIN</h1>
+          <form className="flex  flex-col  items-center h-52 justify-evenly ">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+              required
+            />
+            <button className="w-80 h-9 bg-black rounded-lg ">Wide</button>
+          </form>
+        </div>
       </div>
-    </div>
-
-    
-
-   
     </>
-  )
+  );
 }
