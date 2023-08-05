08e03691fda03eab16d26e75d2c1544bcf105179
WahyuIndG <wahyuig03@gmail.com>
2023-08-05 21:21:12 +0700

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
-=======
->>>>>>> f8f78e84f9c69dfdbab9d25da25ba22382a26102
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Thread.page.jsx
+++ src/pages/Thread.page.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchThreads } from '../redux/threadsSlice';
+import { fetchThreads } from '../redux/reducer/threadsSlice';
 import { useEffect } from 'react';
 
 export default function ThreadPage() {
