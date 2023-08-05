bd669e576011eb43f3a78e7717c96e3005dd0cf0
ddnamn <notimportantfile931900@gmail.com>
2023-08-06 01:35:01 +0700

    thread page style

--- src/pages/Thread.page.jsx
+++ src/pages/Thread.page.jsx
@@ -1,18 +1,32 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchThreads } from '../redux/reducer/threadsSlice';
 import { useEffect } from 'react';
+import { getLeaderboards, getThreads } from '../utils/getData';
+import { useDispatch } from 'react-redux';
+import CardList from '../components/CardList';
+
+import { threads } from '../utils/local-data'; // data-local (arrayOfObject)
 
 export default function ThreadPage() {
-  const listOfThreads = useSelector((store) => store.threads.threadsData);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchThreads());
-  }, []);
-
   return (
     <>
-      <div className="w-2/3 m-auto bg-slate-50">
-        <h1 className="text-black">Thread Page</h1>
+      <div className="threads__container w-2/3 m-auto bg-primary text-transcript py-[1rem]">
+        {/* HEADER */}
+        <header className="threads__header p-[10px] font-medium">
+          <h2 className="text-sm mb-[15px]">Kategori Popular</h2>
+          <div className="category-container flex gap-[15px] text-[0.9rem]">
+            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+              #cewek
+            </button>
+            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+              #ewe
+            </button>
+            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit  py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+              #enaklah
+            </button>
+          </div>
+          <h1 className="text-[1.7rem] font-semibold mt-[20px]">Diskusi tersedia</h1>
+        </header>
+        {/* {LIST} */}
+        <CardList threads={threads} />
       </div>
     </>
   );
