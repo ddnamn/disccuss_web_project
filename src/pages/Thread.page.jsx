96fc8328ce7634bfdd587a9a9627410cac6c4f01
WahyuIndG <wahyuig03@gmail.com>
2023-08-06 13:32:56 +0700

    v

--- src/pages/Thread.page.jsx
+++ src/pages/Thread.page.jsx
@@ -7,22 +7,22 @@ import { threads } from '../utils/local-data'; // data-local (arrayOfObject)
 export default function ThreadPage() {
   return (
     <>
-      <div className="threads__container w-2/3 m-auto bg-primary text-transcript py-[1rem]">
+      <div className="threads__container max-w-[700px] w-[100%] m-auto bg-primary text-transcript py-[1rem]">
         {/* HEADER */}
         <header className="threads__header p-[10px] font-medium">
-          <h2 className="text-sm mb-[15px]">Kategori Popular</h2>
-          <div className="category-container flex gap-[15px] text-[0.9rem]">
-            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+          <h2 className="text-sm mb-[8px]">Kategori Popular</h2>
+          <div className="category-container flex gap-[15px] text-[0.75rem]">
+            <button className="bg-transparent border-solid border-transcript border-[1.35px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
               #cewek
             </button>
-            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+            <button className="bg-transparent border-solid border-transcript border-[1.35px] text-inherit py-[3px] px-[7px] rounded-[7px] cursor-pointer">
               #ewe
             </button>
-            <button className="bg-transparent border-solid border-transcript border-[1.5px] text-inherit  py-[3px] px-[7px] rounded-[7px] cursor-pointer">
+            <button className="bg-transparent border-solid border-transcript border-[1.35px] text-inherit  py-[3px] px-[7px] rounded-[7px] cursor-pointer">
               #enaklah
             </button>
           </div>
-          <h1 className="text-[1.7rem] font-semibold mt-[20px]">Diskusi tersedia</h1>
+          <h1 className="text-[1.7rem] font-bold mt-[20px]">Diskusi tersedia</h1>
         </header>
         {/* {LIST} */}
         <CardList threads={threads} />
