e043d94c0d5e2f31fd465eba8fc175a1bb06cbed
ddnamn <notimportantfile931900@gmail.com>
2023-08-06 15:23:50 +0700

    register logic is solved

--- src/redux/reducer/registerSlice.js
+++ src/redux/reducer/registerSlice.js
@@ -1,36 +1,37 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  status: '',
-  message: '',
+  status: "",
+  message: "",
   data: [],
 };
 
 export const fetchRegisterData = createAsyncThunk(
-  'register/fetchRegisterData',
-  async ({ data }) => {
-    await axios
-      .post('https://forum-api.dicoding.dev/v1/register', JSON.stringify(data))
-      .then((res) => console.log(res.data));
-  }
+  "register/fetchRegisterData",
+  (data) =>
+    axios
+      .post("https://forum-api.dicoding.dev/v1/register", data, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => console.log(res.data))
 );
 
 const registerSlice = createSlice({
-  name: 'register',
+  name: "register",
   initialState,
   extraReducers(builder) {
     builder.addCase(fetchRegisterData.pending, () =>
-      console.log('register proccess is pending')
+      console.log("register proccess is pending")
     );
     builder.addCase(fetchRegisterData.fulfilled, (state, action) => {
       // (state.status = action.status),
       //   (state.message = action.message),
       //   (state.data = action.data.user);
-      console.log('success to register');
+      console.log("success to register");
     });
-    builder.addCase(fetchRegisterData.rejected, () =>
-      console.log('it seems error happen')
+    builder.addCase(fetchRegisterData.rejected, (action) =>
+      console.log("it seems error happen")
     );
   },
 });
