ef92e99656e7827c9bd069a44b03d3aa19d78844
ddnamn <notimportantfile931900@gmail.com>
2023-08-07 17:20:22 +0700

    commit

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Card from './Card';
 
 export default function CardList({ threads }) {
-  console.log(threads)
   return (
     <div className="threads__list w-full flex flex-col justify-center items-center gap-[20px]">
       {threads.map((thread) => (
