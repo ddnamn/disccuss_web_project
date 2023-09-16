1b1a7594424bf8f9f088f3dd1d659cb6d5663ebc
ddnamn <notimportantfile931900@gmail.com>
2023-09-16 17:06:06 +0700

    fix iteration problems in leaderboard page

--- src/components/RankingBoard.jsx
+++ src/components/RankingBoard.jsx
@@ -1,27 +1,38 @@
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
+import { useEffect } from "react"
+import {fetchLearderboards} from "../redux/reducer/leaderboardsSlice"
 
 
-export default function RankingBoard({data}) {
+
+export default function RankingBoard() {
+
+
   const listOfLeaderboard = useSelector((store)=>store.leaderboards.leaderboardsData)
 
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(fetchLearderboards())
+  },[]) 
+
   return (
-   <ul>
-      {/* {listOfLeaderboard.map(ranking=><RankingBoard data={ranking}/>)} */}
-
-      <li  className="flex justify-around items-center p-2">
-         
-         <div className="flex items-center flex-1 justify-start ">
-               <div className="avatar mr-2">
-                  <div className="w-20 rounded-full">
-                     <img src={data.user.avatar} alt="profileImage" />
+   <ul>  
+      {
+      listOfLeaderboard.map(ranking=>
+        <li  className="flex justify-around items-center p-2" key={ranking.user.id}>
+            <div className="flex items-center flex-1 justify-start ">
+                  <div className="avatar mr-2">
+                     <div className="w-20 rounded-full">
+                        <img src={ranking.user.avatar} alt="profileImage" />
+                     </div>
                   </div>
-               </div>
-               
-               <text className="text-2xl font-bold  ml-4">{data.user.name}</text>
-         </div>
+                  
+                  <p className="text-2xl font-bold  ml-4">{ranking.user.name}</p>
+            </div>
 
-         <h3 className="text-2xl font-bold   flex-1 text-center -mr-8">{data.score} </h3>
-      </li>
+                  <h3 className="text-2xl font-bold   flex-1 text-center -mr-8">{ranking.score} </h3>
+         </li>
+      )
+      }
    </ul>
   )
 }
